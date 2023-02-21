c88007e9e07fe3eddd8a984cf699e58a762ebaa1
Ariel Thunstrom <arielthunstrom05@gmail.com>
2023-02-21 09:04:33 -0600

    Initial Commit

---
+++ studyTimerCat/studyTimerCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyTimerCatApp.swift
+//  studyTimerCat
+//
+//  Created by 90309356 on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct studyTimerCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
