b9f401d45c4b6ce57041e9e28ee6ee4c787a0e7d
Ariel Thunstrom <arielthunstrom05@gmail.com>
2023-02-21 09:27:07 -0600

    updating notification exit

--- studyTimerCat/NotificationAppExit.swift
+++ studyTimerCat/NotificationAppExit.swift
@@ -1,8 +1,22 @@
 //
 //  NotificationAppExit.swift
-//  studyTimerCat
+//  studyCat
 //
-//  Created by 90309356 on 2/21/23.
+//  Created by 90310470 on 2/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct NotificationAppExit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationAppExit_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationAppExit()
+    }
+}
+
+
