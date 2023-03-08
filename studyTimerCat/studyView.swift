dca214dc82c52e6f907c43247be561b58accef07
Ariel Thunstrom <arielthunstrom05@gmail.com>
2023-03-08 09:24:46 -0600

    study view

--- studyTimerCat/studyView.swift
+++ studyTimerCat/studyView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct studyView: View {
+    let skyBlue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
     var body: some View {
         ZStack{
-            Color.purple
-                            .ignoresSafeArea()
-            Image("cat2")
+            skyBlue
+                .ignoresSafeArea()
+            VStack {
+                Menu  {
+                    Menu ("hats"){
+                        Button ("Top Hat"){
+                        }
+                    }
+                    Menu ("clothes"){
+                        Button ("Pink Shirt"){
+                        }
+                    }
+                    Menu ("shoes"){
+                        Button ("High Heels"){
+                        }
+                    }
+                    Button("Close", role: .destructive){
+                    }
+                    
+                    
+                } label: {
+                    Label("menu", systemImage: "gearshape.fill")
+                }
+                
+                Spacer()
+                                      
+            
+                        
+               // Image("cat2")
+            }
+            
         }
-        
     }
 }
 
