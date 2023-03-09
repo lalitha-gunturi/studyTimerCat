c685371b4960f99c571262cc373d7e59ba9ecbb7
Sophia Lin <95647522+SophiaLin1505@users.noreply.github.com>
2023-03-09 09:27:00 -0600

    timer in study view

--- studyTimerCat/studyView.swift
+++ studyTimerCat/studyView.swift
@@ -9,47 +9,58 @@ import SwiftUI
 
 struct studyView: View {
     let skyBlue = Color(red: 0.4627, green: 0.8392, blue: 1.0)
+    @StateObject private var vm = ViewModel()
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    private let width: Double = 250
+    
     var body: some View {
-        ZStack{
-            skyBlue
-                .ignoresSafeArea()
-            VStack {
-                Menu  {
-                    Menu ("hats"){
-                        Button ("Top Hat"){
+       
+            
+            ZStack{
+                skyBlue
+                    .ignoresSafeArea()
+                VStack {
+                    Menu  {
+                        Menu ("hats"){
+                            Button ("Top Hat"){
+                            }
                         }
-                    }
-                    Menu ("clothes"){
-                        Button ("Pink Shirt"){
+                        Menu ("clothes"){
+                            Button ("Pink Shirt"){
+                            }
                         }
-                    }
-                    Menu ("shoes"){
-                        Button ("High Heels"){
+                        Menu ("shoes"){
+                            Button ("High Heels"){
+                            }
                         }
-                    }
-                    Button("Close", role: .destructive){
-                    }
-                    
-                    
-                } label: {
-                    Label("menu", systemImage: "gearshape.fill")
-                }
-                
-                Spacer()
-                                      
-                ProgressBar(progress: 50, lineWidth: 15).overlay{
-                         Image("lock")
+                        Button("Close", role: .destructive){
                         }
                         
-               Image("cat2")
+                        
+                    } label: {
+                        Label("menu", systemImage: "gearshape.fill")
+                    }
+                    VStack{
+                        
+                        Text("\(vm.time)")
+                            .font(.system(size: 70, weight: .medium, design: .rounded))
+                            .padding()
+                    Spacer()
+                    
+                    ProgressBar(progress: 50, lineWidth: 15).overlay{
+                        Image("lock")
+                    }
+                    
+                    Image("cat2")
+                }
+                
             }
-            
         }
     }
-}
-
-struct studyView_Previews: PreviewProvider {
-    static var previews: some View {
-        studyView()
+    
+    struct studyView_Previews: PreviewProvider {
+        static var previews: some View {
+            studyView()
+        }
     }
 }
